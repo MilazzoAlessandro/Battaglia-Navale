953ddd246d20d0f45f599b17bd7483948426e276
lai2 <informatica@lai2>
2019-11-13 10:34:34 +0100

    push inserimento sicurezza

--- BattagliaNavale/src/battaglianavale/BattagliaNavale.java
+++ BattagliaNavale/src/battaglianavale/BattagliaNavale.java
@@ -5,6 +5,8 @@
  */
 package battaglianavale;
 
+import java.util.Scanner;
+
 /**
  *
  * @author informatica
@@ -15,20 +17,32 @@ public class BattagliaNavale {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Mappa Map1 = new Mappa(); 
+        Mappa Map1 = new Mappa();
         Mappa Map2 = new Mappa();
         Map2.inizioWater();
         Map1.inizioWater();
-        int c1,c2;
-        }
+        int c1, c2;
+        String d;
+
+        Scanner scan = new Scanner(System.in);
+
         /*Inserire per giocatore 1*/
         System.out.println("inserire barca da 2");
-        Map1.inserimento2(c1,c2);
+        c1 = scan.nextInt();
+        c2 = scan.nextInt();
+        d = scan.next();
+        Map1.inserimento2(c1, c2, d);
         System.out.println("inserire barca da 2");
-        Map1.inserimento2(c1,c2);
+        c1 = scan.nextInt();
+        c2 = scan.nextInt();
+        d = scan.next();
+        Map1.inserimento2(c1, c2, d);
         System.out.println("inserire barca da 3");
-        Map1.inserimento3(c1,c2);
-        
+        c1 = scan.nextInt();
+        c2 = scan.nextInt();
+        d = scan.next();
+        Map1.inserimento3(c1, c2, d);
+
     }
-    
+
 }
