ac92d60235f018534418eded49610b413d907f0f
lai2 <informatica@lai2>
2019-11-20 11:00:07 +0100

    controllo bordi funzionante

--- BattagliaNavale/src/battaglianavale/Mappa.java
+++ BattagliaNavale/src/battaglianavale/Mappa.java
@@ -12,7 +12,7 @@ public class Mappa {
     //ArrayList uso;
 
     public Mappa() {
-        water = new int[8][8];
+        water = new int[10][10];
         conta = 0;
         //uso = new ArrayList();
     }
@@ -20,8 +20,8 @@ public class Mappa {
     public void inizioWater() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if(j==0 || j==10 || i==0 || i==10){
-                    water[i][j]=3;
+                if(j==0 || j==9 || i==0 || i==9){
+                    water[i][j] = 3;
             }else{ water[i][j] = 0;}
             }
         }
@@ -45,30 +45,36 @@ public class Mappa {
 
     public boolean inserimento2(int c1, int c2, String d) {
         if (c1 <= 8 && c2 < 8 && c1 >= 1 && c2 >= 1) {
-            if (d.equalsIgnoreCase("orizzontale")) {
+            if (d.equalsIgnoreCase("orizzontale") || d.equalsIgnoreCase("o")) {
                 if (water[c1][c2] == 0 && water[c1 + 1][c2] == 0 && water[c1 - 1][c2 + 1] != 1
                         && water[c1 - 1][c2] != 1 && water[c1 - 1][c2 - 1] != 1
                         && water[c1][c2 + 1] != 1 && water[c1][c2 - 1] != 1
                         && water[c1 + 1][c2 + 1] != 1 && water[c1 + 1][c2 - 1] != 1
                         && water[c1 + 2][c2 + 1] != 1 && water[c1 + 2][c2] != 1
-                        && water[c1 + 2][c2 - 1] != 1) {
+                        && water[c1 + 2][c2 - 1] != 1)
+                {
+                    
                     water[c1][c2] = 1;
                     water[c1 + 1][c2] = 1;
                     return true;
+              
                 } else {
                      System.out.println("inserire la barca in una cella disponibile \n");
                     return false;
                 }
-            } else if (d.equalsIgnoreCase("verticale")) {
+            } else if (d.equalsIgnoreCase("verticale") || d.equalsIgnoreCase("v")) {
                 if (water[c1][c2] == 0 && water[c1][c2 + 1] == 0
                         && water[c1 - 1][c2 - 1] == 0 && water[c1][c2 - 1] == 0
                         && water[c1 + 1][c2 - 1] == 0 && water[c1 - 1][c2] == 0
                         && water[c1 + 1][c2] == 0 && water[c1 - 1][c2 + 1] == 0
                         && water[c1 + 1][c2 + 1] == 0 && water[c1 - 1][c2 + 2] == 0
-                        && water[c1][c2 + 2] == 0 && water[c1 + 1][c2 + 2] == 0) {
+                        && water[c1][c2 + 2] == 0 && water[c1 + 1][c2 + 2] == 0) 
+                {
+                    
                     water[c1][c2] = 1;
                     water[c1][c2 + 1] = 1;
                     return true;
+                    
                 } else {
                      System.out.println("inserire la barca in una cella disponibile \n");
                     return false;
@@ -85,23 +91,26 @@ public class Mappa {
     
     public boolean inserimento3(int c1, int c2, String d) {
         if (c1 <= 8 && c2 <= 8 && c1 >= 1 && c2 >= 1) {
-            if (d.equalsIgnoreCase("orizzontale")) {
+            if (d.equalsIgnoreCase("orizzontale") || d.equalsIgnoreCase("o")) {
                 if (water[c1][c2] == 0 && water[c1 + 1][c2] == 0 && water[c1 - 1][c2 + 1] != 1
                         && water[c1 - 1][c2] != 1 && water[c1 - 1][c2 - 1] != 1
                         && water[c1][c2 + 1] != 1 && water[c1][c2 - 1] != 1
                         && water[c1 + 1][c2 + 1] != 1 && water[c1 + 1][c2 - 1] != 1
                         && water[c1 + 2][c2 + 1] != 1 && water[c1 + 2][c2] != 1
                         && water[c1 + 2][c2 - 1] != 1 && water[c1 + 3][c2 + 1] != 1
-                        && water[c1 + 3][c2] != 1 && water[c1 + 3][c2 - 1] != 1) {
+                        && water[c1 + 3][c2] != 1 && water[c1 + 3][c2 - 1] != 1) 
+                {
+                    
                     water[c1][c2] = 1;
                     water[c1 + 1][c2] = 1;
                     water[c1 + 2][c2] = 1;
                     return true;
+                    
                 } else {
                      System.out.println("inserire la barca in una cella disponibile \n");
                     return false;
                 }
-            } else if (d.equalsIgnoreCase("verticale")) {
+            } else if (d.equalsIgnoreCase("verticale") || d.equalsIgnoreCase("v")) {
                 if (water[c1][c2] == 0 && water[c1][c2 + 1] == 0
                         && water[c1 - 1][c2 - 1] == 0 && water[c1][c2 - 1] == 0
                         && water[c1 + 1][c2 - 1] == 0 && water[c1 - 1][c2] == 0
@@ -109,11 +118,14 @@ public class Mappa {
                         && water[c1 + 1][c2 + 1] == 0 && water[c1 - 1][c2 + 2] == 0
                         && water[c1][c2 + 2] == 0 && water[c1 + 1][c2 + 2] == 0
                         && water[c1 - 1][c2 + 3] == 0 && water[c1][c2 + 3] == 0
-                        && water[c1 + 1][c2 + 3] == 0) {
+                        && water[c1 + 1][c2 + 3] == 0) 
+                {
+                    
                     water[c1][c2] = 1;
                     water[c1 + 1][c2] = 1;
                     water[c1 + 2][c2] = 1;
                     return true;
+                
                 } else {
                      System.out.println("inserire la barca in una cella disponibile \n");
                     return false;
